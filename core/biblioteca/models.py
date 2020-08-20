9383577e077bd0e5abe4988d12fca85f125724aa
licholucero <licholucero7@gmail.com>
2020-08-20 18:22:38 -0300

    Clase Libro

--- core/biblioteca/models.py
+++ core/biblioteca/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Libro(Material):
+    editorial = models.CharField(max_length=40)
+    fotoPortada = models.ImageField(max_length=100, upload_to='portadas/', blank=True)
